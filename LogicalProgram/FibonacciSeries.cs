43c5f02341cf1d5835f27310bbe09e3daf3f4135
rishiraj007 <rishiraj@mounttalent.com>
2022-12-01 09:07:04 +0530

    [Prabhu]: Fibonacci Series

---
+++ LogicalProgram/FibonacciSeries.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    internal class FibonacciSeries
+    {
+        public void Print(int num)
+        {
+            int firstNum = 0, secondNum = 1, thirdNum = 0;
+            Console.WriteLine(firstNum + "\n" + secondNum);
+            for (int i = 0; i < num; i++)
+            {
+                thirdNum = firstNum + secondNum;
+                firstNum = secondNum;
+                secondNum = thirdNum;
+                Console.WriteLine(thirdNum);
+            }
+        }
+    }
+}
--- LogicalProgram/Program.cs
+++ LogicalProgram/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nEnter your choice \n1.Fibonacci Series \n7.Exit");
+                Console.WriteLine("\nEnter option to execute");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FibonacciSeries series = new FibonacciSeries();
+                        series.Print(5);
+                        break;
+                    default:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
