3223da877afeea8830594a7116923c77399d8a8e
rishiraj007 <rishiraj@mounttalent.com>
2022-12-01 09:15:32 +0530

    [Prabhu]: Prime Number

--- LogicalProgram/Program.cs
+++ LogicalProgram/Program.cs
@@ -7,7 +7,7 @@
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("\nEnter your choice \n1.Fibonacci Series \n2.Perfect Number \n7.Exit");
+                Console.WriteLine("\nEnter your choice \n1.Fibonacci Series \n2.Perfect Number \n3.Prime Number \n7.Exit");
                 Console.WriteLine("\nEnter option to execute");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
@@ -19,6 +19,10 @@
                     case 2:
                         PerfectNumber perfectNumber = new PerfectNumber();
                         perfectNumber.Check(10);
+                        break;
+                    case 3:
+                        PrimeNumber primeNumber = new PrimeNumber();
+                        primeNumber.CheckPrime(7);
                         break;
                     default:
                         flag = false;
